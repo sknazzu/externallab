60b09134d09e865d760604c04c38d8a8c6d0b8fb
Admin <Admin@DESKTOP-MKO248Q>
2023-11-07 11:06:06 +0530

    change

--- src/com/lbrce/external/sample2.java
+++ src/com/lbrce/external/sample2.java
@@ -4,7 +4,8 @@ public class sample2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+System.out.println("this is from class2");
+System.out.println("change");
 	}
 
 }
